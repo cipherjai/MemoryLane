50931899efbd24c3204041b7b576bf906ccabf5f
cipherjai <jaiatmart@gmail.com>
2016-04-17 04:27:08 +0530

    Added Calendar Activity

--- MemoryLane/app/src/main/java/com/firefly/team/memorylane/Welcome.java
+++ MemoryLane/app/src/main/java/com/firefly/team/memorylane/Welcome.java
@@ -1,13 +1,34 @@
 package com.firefly.team.memorylane;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
+
+    private static Button  button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        OnclickButtonListener();
     }
+
+    public void OnclickButtonListener()
+    {
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getBaseContext(),Home_Calendar.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
