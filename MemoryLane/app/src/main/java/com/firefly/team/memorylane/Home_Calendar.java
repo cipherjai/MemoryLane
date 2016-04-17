1253b31f57bb19e2a4531985157638b0695a902c
cipherjai <jaiatmart@gmail.com>
2016-04-17 11:38:00 +0530

    Added Calendar

--- MemoryLane/app/src/main/java/com/firefly/team/memorylane/Home_Calendar.java
+++ MemoryLane/app/src/main/java/com/firefly/team/memorylane/Home_Calendar.java
@@ -1,13 +1,16 @@
 package com.firefly.team.memorylane;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CalendarView;
+//import android.widget.CalendarView;
 
-public class Home_Calendar extends AppCompatActivity {
+public class Home_Calendar extends Activity implements OnDateChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,9 @@ public class Home_Calendar extends AppCompatActivity {
         setContentView(R.layout.activity_home__calendar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        CalendarView cw = (CalendarView)findViewById(R.id.calendarView);
+        cw setOnDateChangeListener(Home_Calendar.this);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +30,10 @@ public class Home_Calendar extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+    @Override
+    public void onSelectedDayChange(CalenderView arg0,int arg1, int arg2, int arg 3){
+
     }
 
 }
