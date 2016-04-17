07fb8480365fe1121810df9946e47a950bb76c99
cipherjai <jaiatmart@gmail.com>
2016-04-17 15:28:55 +0530

    Added Calendar Furher

--- MemoryLane/app/src/main/java/com/firefly/team/memorylane/Home_Calendar.java
+++ MemoryLane/app/src/main/java/com/firefly/team/memorylane/Home_Calendar.java
@@ -7,10 +7,21 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 //import android.widget.CalendarView;
 
 public class Home_Calendar extends AppCompatActivity implements  CalendarView.OnDateChangeListener {
+
+    // Array of options
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,11 @@ public class Home_Calendar extends AppCompatActivity implements  CalendarView.On
         setSupportActionBar(toolbar);
         CalendarView cw = (CalendarView)findViewById(R.id.calendarView);
         cw.setOnDateChangeListener(Home_Calendar.this);
+
+        populateListView();
+        registerClickCallBack();
+
+
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -31,11 +47,37 @@ public class Home_Calendar extends AppCompatActivity implements  CalendarView.On
             }
         });
     }
+    private void populateListView()
+    {
+        // Create list
+        String [] myItems = {"Blue","Green","Purple","Red"};
+
+        // Adapter Build
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.da_item,myItems);
+
+        //Configure
+
+        ListView list = (ListView)findViewById(R.id.listView);
+        list.setAdapter(adapter);
+    }
+    private void registerClickCallBack()
+    {
+        ListView list = (ListView) findViewById(R.id.listView);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent , View viewClicked, int position, long id){
+                TextView textView = (TextView) viewClicked;
+                String message = "You Clicked #" + (position+1) + ", which is string " + textView.getText().toString();
+                Toast.makeText(Home_Calendar.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
 
 
     @Override
     public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
 
-
+        Toast.makeText(Home_Calendar.this,year + " " + month + " " + dayOfMonth,Toast.LENGTH_SHORT).show();
     }
 }
